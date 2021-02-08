d5c69abffbf3e63576c5923b9b7deeceb9e6c114
lisgroup <lisgroup@qq.com>
2021-02-08 14:52:41 +0800

    chore: 天翼翼证-区块链 验签服务

---
+++ README.md
@@ -0,0 +1,42 @@
+## 天翼翼证-区块链 验签服务
+
+### 一、启动方法
+1. run
+```shell script
+go run main.go
+```
+2. build
+```shell script
+go build main.go
+./main
+```
+
+### 二、 客户端获取sign
+
+- 请求地址设定为 http://127.0.0.1:9898/
+
+- 请求参数列表
+
+参数名称 | 描述
+---|---
+body | 接口参数
+nonce | 签名内容里面的随机数
+timeStamp | 签名内容里面的时间戳
+apiSecret | 秘钥
+
+
+完整请求示例：
+```
+http://127.0.0.1:9898/sign?body={"Data":"test","Remark":"test"}&nonce=100&timeStamp=1608626661&apiSecret=482e4610af93b60a113a8ecc61a500eaf054ee6df030ac2ece669acbe58ec28e
+```
+响应JSON示例：
+
+```json
+{
+	"code": 0,
+	"data": {
+		"sign": "40677972351786153192903865501230545775316569974084416717902566734041634855574+24262690419852203592204540437043224196359361711282558324821312146479811246097"
+	},
+	"reason": "success"
+}
+```
---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"github.com/gin-gonic/gin"
+	"io"
+	"math/big"
+	"os"
+	"strconv"
+)
+
+func main() {
+	// 关闭console日志
+	gin.DisableConsoleColor()
+	// 记录为文件日志
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f)
+	// 设置模式
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
+	r.GET("/sign", func(c *gin.Context) {
+		// 接收四个参数 接口参数 + nonce + timeStamp + apiSecret
+		body := c.DefaultQuery("body", "")
+		// 随机数
+		Nonce := c.DefaultQuery("nonce", "")
+		TimeStamp := c.DefaultQuery("timeStamp", "")
+		apiSecret := c.DefaultQuery("apiSecret", "")
+
+		if body == "" || Nonce == "" || TimeStamp == "" || apiSecret == "" {
+			c.JSON(200, gin.H{
+				"code":   400,
+				"data":   nil,
+				"reason": "error",
+			})
+			return
+		}
+		timeStamp, _ := strconv.Atoi(TimeStamp)
+		signStr := body + Nonce + strconv.FormatInt(int64(timeStamp), 10)
+		// fmt.Printf("签名字符串： %s \n", signStr)
+		sign := sign(signStr, apiSecret)
+		// fmt.Printf("签名结果：%s \n", sign)
+		// gin.LoggerWithWriter(f, "签名字符串： %s \n", signStr)
+		c.JSON(200, gin.H{
+			"code":   0,
+			"data":   map[string]string{"sign": sign},
+			"reason": "success",
+		})
+	})
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "hello")
+	})
+	_ = r.Run(":9898") // listen and serve on 0.0.0.0:8080
+}
+
+// sign 签名
+func sign(str string, privateKey string) string {
+	str = toHash256(str)
+	getPrivateKey := stringToPrivateKey(privateKey)
+	bys := []byte(str)
+	r, s, err := ecdsa.Sign(rand.Reader, getPrivateKey, bys)
+	if err != nil {
+		return ""
+	}
+	rb := hex.EncodeToString(r.Bytes())
+	sb := hex.EncodeToString(s.Bytes())
+	return rb + sb
+}
+
+// toHash256 对请求内容hash
+func toHash256(str string) string {
+	hash := sha256.New()
+	hash.Write([]byte(str))
+	return string(hash.Sum([]byte(nil)))
+}
+
+// stringTOPrivateKey string->私钥
+func stringToPrivateKey(str string) *ecdsa.PrivateKey {
+	var newCurve = elliptic.P256()
+	prv, _ := hex.DecodeString(str)
+	if len(prv) == 0 {
+		return nil
+	}
+	privateKey := new(ecdsa.PrivateKey)
+	privateKey.PublicKey.Curve = newCurve
+	privateKey.D = new(big.Int).SetBytes(prv)
+	privateKey.PublicKey.X, privateKey.PublicKey.Y = privateKey.PublicKey.Curve.ScalarBaseMult(prv)
+	return privateKey
+}
